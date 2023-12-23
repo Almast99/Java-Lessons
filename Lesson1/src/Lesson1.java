bc72802399dd7b47f623bfe6411118d0cfc5ecb7
Almast99 <137831437+Almast99@users.noreply.github.com>
2023-12-23 21:26:49 +0400

    My first git commit

---
+++ Lesson1/src/Lesson1.java
@@ -0,0 +1,25 @@
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Barev");
+        System.out.println("Lesson1");
+
+        byte x = 127;
+        System.out.println(x);
+
+        short y = -4000;
+        System.out.println(y);
+
+        int a = 85555555;
+        System.out.println(a);
+
+        long z = 52145L;
+        System.out.println(z);
+        char s='$';
+        System.out.println(s);
+        boolean v = true;
+        v = false;
+        System.out.println(v);
+
+
+    }
+}
