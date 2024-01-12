2d48add0ec4eb33acdf95bef48f4dfa1064e2d39
Almast Movsisyan <almast.movsisyan99@gmail.com>
2024-01-12 23:06:36 +0400

    Add new file

---
+++ Lesson2/src/Operations.java
@@ -0,0 +1,80 @@
+public class Operations {
+    public static void main(String[] args) {
+
+        int _x = 10;
+        System.out.println(_x);
+
+        int y;
+        y = 20;
+        System.out.println(y);
+        int h12 = 60;
+        System.out.println(h12);
+
+        String firstName = "Almast";
+        System.out.println(firstName);
+        String firstNameOfTheBestStudent = "Almast";
+        System.out.println(firstNameOfTheBestStudent);
+
+        int a = 4, b = 5;
+        int c = a + b;
+        System.out.println(c);
+
+        c = a - b;
+        System.out.println(c);
+
+        System.out.println(a*b);
+
+        int w = 7, q = 5;
+        System.out.println(w/q);
+        System.out.println(w%q);
+
+        int x = 4;
+        x++;
+        System.out.println(x);
+
+        x = 3;
+        y = 4;
+        c = x++ - --y;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("c = " + c);
+
+
+        boolean bb = x == 4;
+        System.out.println(bb);
+
+        System.out.println(x != 45);
+
+        x = 40;
+        System.out.println(x % 2 == 0);
+
+
+
+
+        double xxx =  7, yyy = 5;
+        System.out.println(xxx/yyy);
+
+
+
+        boolean p = x >= 1 && x <= 9;
+        System.out.println(p);
+
+        int cc = 100;
+
+        boolean ss = cc >= 100 && cc <= 1;
+        System.out.println(ss);
+
+
+        System.out.println("----------");
+
+        System.out.println(5&8);
+
+
+
+
+
+
+
+    }
+
+}
